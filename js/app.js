64058db3cdfd1861292dd3a99e05da8643ac51cb
Mirjahon96 <auden.05@mail.ru>
2020-05-27 02:49:24 +0500

    adding toolbar and showcase

--- js/app.js
+++ js/app.js
@@ -3,7 +3,19 @@ new Vue({
   vuetify: new Vuetify(),
   data() {
     return {
-      drawer: false
+      drawer: false,
+      // App Bar
+      links: [
+        { name: "Home", href: "#Vassland" },
+        { name: "About", href: "#about" },
+        { name: "Services", href: "#services" },
+        { name: "Projects", href: "#projects" },
+        { name: "Contact", href: "#contact" }
+      ],
+      // Services
+      servicesSelect: ".com",
+      names: [".com", ".net", ".org", ".uz", ".uk"]
     };
-  }
+  },
+  methods: {}
 });
