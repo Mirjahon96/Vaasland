d62adf130e40f795fe33bf4e942c609339d5fb25
Mirjahon96 <auden.05@mail.ru>
2020-06-07 00:06:57 +0500

    adding pricing section

--- js/app.js
+++ js/app.js
@@ -62,6 +62,87 @@ new Vue({
           text:
             "Hunky dory barney fanny around up the duff no biggie loo cup of tea jolly good ruddy say arse.!"
         }
+      ],
+
+      // Pricing
+      pricingTab: null,
+      priceHeaders: [
+        {
+          text: "Memory",
+          align: "center",
+          value: "memory"
+        },
+        { text: "Vspus", align: "center", value: "vspus" },
+        { text: "SSD Disk", align: "center", value: "ssdDisk" },
+        { text: "Transfer", align: "center", value: "transfer" },
+        { text: "Price", align: "center", value: "price" },
+        { text: "Plan", align: "center", value: "plan" }
+      ],
+      dataPrice: [
+        {
+          memory: "1 GB",
+          vspus: "1 vCPU",
+          ssdDisk: "125GB",
+          transfer: "1 TB",
+          price: "$10/mo $0.025/hr",
+          plan: "Choose Plan"
+        },
+        {
+          memory: "2 GB",
+          vspus: "2 vCPU",
+          ssdDisk: "100GB",
+          transfer: "2 TB",
+          price: "$15/mo $0.025/hr",
+          plan: "Choose Plan"
+        },
+        {
+          memory: "3 GB",
+          vspus: "3 vCPU",
+          ssdDisk: "50GB",
+          transfer: "1 TB",
+          price: `$25/mo $0.025/hr`,
+          plan: "Choose Plan"
+        },
+        {
+          memory: "4 GB",
+          vspus: "4 vCPU",
+          ssdDisk: "120GB",
+          transfer: "4 TB",
+          price: `$75/mo $0.725/hr`,
+          plan: "Choose Plan"
+        },
+        {
+          memory: "8 GB",
+          vspus: "5 vCPU",
+          ssdDisk: "120GB",
+          transfer: "5 TB",
+          price: `$75/mo $0.725/hr`,
+          plan: "Choose Plan"
+        },
+        {
+          memory: "16 GB",
+          vspus: "6 vCPU",
+          ssdDisk: "200GB",
+          transfer: "6 TB",
+          price: `$120/mo $0.160/hr`,
+          plan: "Choose Plan"
+        },
+        {
+          memory: "32 GB",
+          vspus: "7 vCPU",
+          ssdDisk: "260GB",
+          transfer: "4 TB",
+          price: `$150mo $0.125/hr`,
+          plan: "Choose Plan"
+        },
+        {
+          memory: "8 GB",
+          vspus: "3 vCPU",
+          ssdDisk: "120GB",
+          transfer: "1 TB",
+          price: `$25/mo $0.025/hr`,
+          plan: "Choose Plan"
+        }
       ]
     };
   },
